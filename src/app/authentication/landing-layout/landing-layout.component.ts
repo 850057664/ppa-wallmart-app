fea7708ee8e66f10bec453817e4806d181157fcd
Kumari <850057664@genpact.com>
2022-01-31 14:06:00 +0530

    initial push

--- src/app/authentication/landing-layout/landing-layout.component.ts
+++ src/app/authentication/landing-layout/landing-layout.component.ts
@@ -9,181 +9,246 @@ declare var $: any;
   templateUrl: './landing-layout.component.html',
 })
 export class LandingLayoutComponent implements OnInit {
-
-
   radioValue;
-  NextBtnDisable:boolean = true;
+  NextBtnDisable: boolean = true;
+  showPRRequest: boolean = true;
+  showNextFnCall: boolean = false;
 
-  listOfData = []
 
+  listOfData = [];
 
-  disable_next_btn: boolean = true
-  constructor(private router: Router,
-              private message: NzMessageService
-  ) { }
-
+  disable_next_btn: boolean = true;
+  constructor(private router: Router, private message: NzMessageService) {}
 
   ngOnInit(): void {
     var elementData = document.getElementById('data');
     var elementClaim = document.getElementById('claim');
     var elementReports = document.getElementById('reports');
     var elemenAudit = document.getElementById('audit');
-    elementData.classList.remove("cardActive");
-    elementClaim.classList.remove("cardActive");
-    elementReports.classList.remove("cardActive");
-    elemenAudit.classList.add("cardActive");
+    elementData.classList.remove('cardActive');
+    elementClaim.classList.remove('cardActive');
+    elementReports.classList.remove('cardActive');
+    elemenAudit.classList.add('cardActive');
 
-    this.listOfData.push({
-      name: `Volume Rebet`,
-    },
+    this.listOfData.push(
+      {
+        name: `Volume Rebet`,
+      },
       {
         name: `New Store`,
       },
       {
+        name: `Undeducted Co-op`,
+      },
+
+      {
+        name: `Miscellaneous`,
+      },
+      {
+        name: `Scan Allowance`,
+      },
+      {
+        name: `Duplicate`,
+      },
+      {
+        name: `Advertising`,
+      },
+      {
+        name: `Shortages`,
+      },
+      {
+        name: `Low Price`,
+      },
+      {
+        name: `Fill Rate`,
+      },
+
+      {
         name: `Deducted Co-op`,
-      },
-
-      {
-        name: `Placeholder`,
-      },
-      {
-        name: `demo 3`,
-      },
-    )
+      }
+    );
   }
 
-  showNav(show) {
-
-  }
+  showNav(show) {}
 
   selectPPA(e) {
-
-    this.listOfData = []
-    console.log(e)
-    if (e == "data") {
-
+    this.listOfData = [];
+    console.log(e);
+    if (e == 'data') {
+      this.showNextFnCall = true;
+      this.showPRRequest = false;
       var elementData = document.getElementById('data');
       var elementClaim = document.getElementById('claim');
       var elementReports = document.getElementById('reports');
       var elemenAudit = document.getElementById('audit');
-      elementData.classList.add("cardActive");
-      elementClaim.classList.remove("cardActive");
-      elementReports.classList.remove("cardActive");
-      elemenAudit.classList.remove("cardActive");
+      elementData.classList.add('cardActive');
+      elementClaim.classList.remove('cardActive');
+      elementReports.classList.remove('cardActive');
+      elemenAudit.classList.remove('cardActive');
 
-      this.listOfData.push({
-        name: `Volume Rebet`,
-      },
+      this.listOfData.push(
+        {
+          name: `Volume Rebet`,
+        },
         {
           name: `New Store`,
         },
         {
-          name: `Deducted Co-op`,
+          name: `Undeducted Co-op`,
         },
 
         {
-          name: `Placeholder`,
+          name: `Scan Allowance`,
         },
-      )
-    }
-    else if (e == "claim") {
+        {
+          name: `Duplicate`,
+        },
+        {
+          name: `Advertising`,
+        },
+        {
+          name: `Shortages`,
+        },
+        {
+          name: `Low Price`,
+        },
+        {
+          name: `Fill Rate`,
+        },
+
+        {
+          name: `Deducted Co-op`,
+        }
+      );
+    } else if (e == 'claim') {
+      this.showNextFnCall = true;
+      this.showPRRequest = false;
       var elementData = document.getElementById('data');
       var elementClaim = document.getElementById('claim');
       var elementReports = document.getElementById('reports');
       var elemenAudit = document.getElementById('audit');
-      elementClaim.classList.add("cardActive");
-      elementData.classList.remove("cardActive");
-      elementReports.classList.remove("cardActive");
-      elemenAudit.classList.remove("cardActive");
+      elementClaim.classList.add('cardActive');
+      elementData.classList.remove('cardActive');
+      elementReports.classList.remove('cardActive');
+      elemenAudit.classList.remove('cardActive');
 
+      this.listOfData.push(
 
-      this.listOfData.push({
-        name: `claim one`,
-      },
-        {
-          name: `claim two`,
-        },
-        {
-          name: `Placeholder`,
-        },
-        {
-          name: `Deducted Co-op`,
-        })
-    }
-    else if (e == "reports") {
-
+      );
+    } else if (e == 'reports') {
+      this.showNextFnCall = true;
+      this.showPRRequest = false;
       var elementData = document.getElementById('data');
       var elementClaim = document.getElementById('claim');
       var elementReports = document.getElementById('reports');
       var elemenAudit = document.getElementById('audit');
-      elementReports.classList.add("cardActive");
-      elementData.classList.remove("cardActive");
-      elementClaim.classList.remove("cardActive");
-      elemenAudit.classList.remove("cardActive");
+      elementReports.classList.add('cardActive');
+      elementData.classList.remove('cardActive');
+      elementClaim.classList.remove('cardActive');
+      elemenAudit.classList.remove('cardActive');
 
-      this.listOfData.push({
-        name: `Reports one`,
-      },
+      this.listOfData.push(
         {
-          name: `Reports two`,
+          name: `Claims Consolidated`,
         },
         {
-          name: `Placeholder`,
+          name: `Claim Tracking Details`,
         },
         {
-          name: `Deducted Co-op`,
-        })
-    }
-    else if (e == "audit") {
-
+          name: `Claims List Pending`,
+        },
+      );
+    } else if (e == 'audit') {
+      this.showNextFnCall = false;
+      this.showPRRequest = true;
       var elementData = document.getElementById('data');
       var elementClaim = document.getElementById('claim');
       var elementReports = document.getElementById('reports');
       var elemenAudit = document.getElementById('audit');
-      elemenAudit.classList.add("cardActive");
-      elementData.classList.remove("cardActive");
-      elementReports.classList.remove("cardActive");
-      elementClaim.classList.remove("cardActive");
+      elemenAudit.classList.add('cardActive');
+      elementData.classList.remove('cardActive');
+      elementReports.classList.remove('cardActive');
+      elementClaim.classList.remove('cardActive');
 
-      this.listOfData.push({
-        name: `Audit one`,
-      },
+      this.listOfData.push(
         {
-          name: `Audit two`,
+          name: `Volume Rebet`,
         },
         {
-          name: `Placeholder`,
+          name: `New Store`,
+        },
+        {
+          name: `Undeducted Co-op`,
+        },
+
+        {
+          name: `Miscellaneous`,
+        },
+        {
+          name: `Scan Allowance`,
+        },
+        {
+          name: `Duplicate`,
+        },
+        {
+          name: `Advertising`,
+        },
+        {
+          name: `Shortages`,
+        },
+        {
+          name: `Low Price`,
+        },
+        {
+          name: `Fill Rate`,
+        },
+        {
+          name: `Fill Rate`,
         },
         {
           name: `Deducted Co-op`,
-        })
+        }
+      );
     }
-
   }
 
   changeAudiType(e) {
     this.radioValue = e;
-    console.log(e, "hhh1")
+    console.log(e, 'hhh1');
 
     this.NextBtnDisable = false;
   }
 
-  sendRequest(): void {
-
-    console.log(this.radioValue, "hhh")
-    if ( this.radioValue !== undefined) {
+  ManagePrFnCall(): void {
+    console.log(this.radioValue, 'hhh');
+    if (this.radioValue !== undefined) {
       this.router.navigate(['/dashboard/home']);
-    }
-    else{
+    } else {
       this.NextBtnDisable = false;
       // this.message.create('error', `Please select Audit type!`);
     }
-
-
   }
 
-  LogoutUser(){
+  AssignPRFnCall(): void {
+    console.log(this.radioValue, 'hhh');
+    if (this.radioValue !== undefined) {
+      this.router.navigate(['/dashboard/home']);
+    } else {
+      this.NextBtnDisable = false;
+      // this.message.create('error', `Please select Audit type!`);
+    }
+  }
+
+  NextFnCall(){
+    if (this.radioValue !== undefined) {
+      this.router.navigate(['/dashboard/home']);
+    } else {
+      this.NextBtnDisable = false;
+      // this.message.create('error', `Please select Audit type!`);
+    }
+  }
+
+  LogoutUser() {
     this.router.navigate(['/authentication/login']);
   }
 }
-
